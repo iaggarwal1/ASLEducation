a29d19611fb01e0f7e32163635b3fcc21a1493b5
Ibhan Aggarwal <ibhan2005@gmail.com>
2024-03-29 02:10:22 -0400

    Connect to MongoDB

--- api/server.js
+++ api/server.js
@@ -1,8 +1,21 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}..`)
+  console.log(`Listening on port ${port}..`);
 });
