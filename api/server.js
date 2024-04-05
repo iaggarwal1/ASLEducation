173d06c1e8b52f6cfe25c54c8a4aa328990460b8
Ibhan Aggarwal <ibhan2005@gmail.com>
2024-04-05 17:39:53 -0400

    Prepare for deployment

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -15,7 +16,14 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
