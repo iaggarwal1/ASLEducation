6164d312dabcfa4edaddc5fa0556f6337f358590
Ibhan Aggarwal <ibhan2005@gmail.com>
2024-04-09 18:44:44 -0400

    Configure Vite for Build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { mediapipe } from "vite-plugin-mediapipe";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mediapipe()],
   server: {
     proxy: {
       "/api": {
