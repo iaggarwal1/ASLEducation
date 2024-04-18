f16bdb694eff74fea20689eed884a44988772ebd
Ibhan Aggarwal <ibhan2005@gmail.com>
2024-04-17 20:32:58 -0400

    Fix AWS Endpoint

--- api/controllers/feed.controller.js
+++ api/controllers/feed.controller.js
@@ -1,6 +1,6 @@
 export const frames = async (req, res, next) => {
   try {
-    const response = await fetch(AWS_DNS, {
+    const response = await fetch(process.env.AWS_DNS, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
